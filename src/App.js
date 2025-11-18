53dedc7161f0838cc02760b6e7248c3e3ebeda9a
ThaiSon1708 <son0987801073@gmail.com>
2025-11-18 15:20:02 +0700

    Clean restart: Ready to push

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'; 
+import StudentTable from './StudentTable'; 
+function App() {
+  return (
+    <div className="App">
+      <header>
+      </header>   
+      <StudentTable />   
+    </div>
+  );
+}
+
+export default App;
